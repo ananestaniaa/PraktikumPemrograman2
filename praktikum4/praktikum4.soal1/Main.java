c78bfd6edf72d8c060df871ac1a36b66dcbba940
ananestaniaa <114906076+ananestaniaa@users.noreply.github.com>
2022-11-16 05:09:30 +0800

    Add files via upload

---
+++ praktikum4/praktikum4.soal1/Main.java
@@ -0,0 +1,28 @@
+package praktikum4.soal1;
+/**
+ *
+ * @author ANA NESTANIA
+ */
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String judul = "", penulis = "";
+        int tahun = 0;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Judul: ");
+        judul = input.nextLine();
+        
+        System.out.print("Penulis: ");
+        penulis = input.nextLine();
+        
+        System.out.print("Tahun Terbit: ");
+        tahun = input.nextInt();
+        
+        Buku buku = new Buku(judul, penulis, tahun);
+        
+        buku.display(); 
+    }
+}
---
+++ praktikum4/praktikum4.soal2/Buku.java
@@ -0,0 +1,11 @@
+package praktikum4.soal2;
+/**
+ *
+ * @author ANA NESTANIA
+ */
+public abstract class Buku {
+    protected String judul, penulis;
+    protected int tahun;
+
+    protected void display(){} 
+}
