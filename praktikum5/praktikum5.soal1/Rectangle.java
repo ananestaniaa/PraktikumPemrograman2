db989b40fc706d4310387ab7c9eb78544e964609
ananestaniaa <114906076+ananestaniaa@users.noreply.github.com>
2022-11-30 05:42:23 +0800

    Add files via upload

---
+++ praktikum5/praktikum5.soal1/Rectangle.java
@@ -0,0 +1,36 @@
+package praktikum5.soal1;
+
+/**
+ *
+ * @author ANA NESTANIA
+ */
+public class Rectangle extends Shape {
+    private double length;
+    private double width;
+    
+    //------------------------------------     
+    // Constructor: Sets up the rectangle. 
+    //------------------------------------ 
+
+    public Rectangle(double l, double w){
+        super("Rectangle");
+        length = l;
+        width = w;
+    }
+    
+    //-------------------------------------------- 
+    // Returns the surface area of the rectangle. 
+    //-------------------------------------------- 
+
+    public double area(){
+        return length * width;
+    }
+    
+    //------------------------------------ 
+    // Returns the rectangle as a String. 
+    //------------------------------------ 
+     
+    public String toString(){
+        return super.toString() + " of length " + length + " and width " + width;
+    }
+}
