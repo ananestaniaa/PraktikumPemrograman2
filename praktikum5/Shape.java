06f64b3096b6287944e1a7fae6673f00b70e7dc2
ananestaniaa <114906076+ananestaniaa@users.noreply.github.com>
2022-11-30 05:46:53 +0800

    Add files via upload

---
+++ praktikum5/Shape.java
@@ -0,0 +1,31 @@
+package praktikum5.soal1;
+
+/**
+ *
+ * @author ANA NESTANIA
+ */
+public abstract class Shape {
+    protected String shapeName;
+    
+    //------------------------------------ 
+    // Constructor: Sets up the shape. 
+    //------------------------------------ 
+    
+    public Shape(String name){
+        shapeName = name;
+    }
+    
+    //-------------------------------------------- 
+    // Returns the surface area of the shape. 
+    //-------------------------------------------- 
+
+    protected abstract double area(); 
+     
+    //------------------------------------ 
+    // Returns the shape as a String. 
+    //------------------------------------ 
+
+    public String toString(){
+        return shapeName;
+    }
+}
